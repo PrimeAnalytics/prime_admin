fd7d6ac5b00b8d4c808ca11ba289d93f442aa62a
WIN-HPK45MV22AU\Administrator <support@primeanalytics.io>
2015-09-17 14:27:37 -0700

    router update

---
+++ app/form_element/FormElementBase.php
@@ -0,0 +1,22 @@
+<?php
+namespace PRIME\FormElement;
+
+class FormElementBase extends Controller
+{ 
+    public function renderAction()
+    {
+    
+    }
+
+    public function newAction()
+    {
+    
+    }
+
+    public function editAction()
+    {
+
+    }
+
+
+}
